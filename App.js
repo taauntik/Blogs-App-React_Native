c745ca71dc03eb5e4dc4dd08f224f945d1e4cf54
taauntik <devauntik@gmail.com>
2021-06-23 19:51:20 +0600

    start my project

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+
+import { BlogPost } from "./src/features/blogPost/screens/blog-post.screen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <BlogPost />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/features/blogPost/screens/blog-post.screen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text } from "react-native";
+import { SafeArea } from "../components/safeArea/safe-area.component";
+
+export const BlogPost = () => {
+  return (
+    <SafeArea>
+      <Text>Hello World this is the blogPost screen</Text>
+    </SafeArea>
+  );
+};
